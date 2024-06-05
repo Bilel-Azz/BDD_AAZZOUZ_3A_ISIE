f23c3f6050ee58b222c6f817711b3e4bd64a7454
Bilel-Azz <113582489+Bilel-Azz@users.noreply.github.com>
2024-06-05 17:15:22 +0200

    Update 2024_BDD_AAZZOUZ_BILEL_EX8.sql

--- 2024_BDD_AAZZOUZ_BILEL_EX8/2024_BDD_AAZZOUZ_BILEL_EX8.sql
+++ 2024_BDD_AAZZOUZ_BILEL_EX8/2024_BDD_AAZZOUZ_BILEL_EX8.sql
@@ -1,31 +1,31 @@
 -- Création de la table Disponibilite
 CREATE TABLE Disponibilite (
-    ID_Disponibilite INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    ID_Materiel INT,
-    DateDebut DATE,
-    DateFin DATE,
-    FOREIGN KEY (ID_Materiel) REFERENCES Materiel(ID_Materiel)
+    ID_Disponibilite INT PRIMARY KEY NOT NULL AUTO_INCREMENT, -- Identifiant de la disponibilité
+    ID_Materiel INT, -- Identifiant du matériel, clé étrangère vers Materiel
+    DateDebut DATE, -- Date de début de la disponibilité 
+    DateFin DATE, -- Date de fin de la disponibilité
+    FOREIGN KEY (ID_Materiel) REFERENCES Materiel(ID_Materiel) -- Clé étrangère vers la table Materiel
 );
 
 -- Ajout de la colonne ID_Disponibilite dans la table Reservation
-ALTER TABLE Reservation
-ADD COLUMN ID_Disponibilite INT,
-ADD FOREIGN KEY (ID_Disponibilite) REFERENCES Disponibilite(ID_Disponibilite);
+ALTER TABLE Reservation -- Ajout de la colonne ID_Disponibilite
+ADD COLUMN ID_Disponibilite INT, -- Identifiant de la disponibilité, clé étrangère vers Disponibilite
+ADD FOREIGN KEY (ID_Disponibilite) REFERENCES Disponibilite(ID_Disponibilite); -- Clé étrangère vers la table Disponibilite
 
 -- Déclencheur avant l'insertion pour vérifier la disponibilité du matériel
 DELIMITER //
 
-CREATE TRIGGER Before_Reservation_Insert
-BEFORE INSERT ON Reservation
-FOR EACH ROW
-BEGIN
-    DECLARE AvailCount INT;
-    SELECT COUNT(*)
-    INTO AvailCount
-    FROM Disponibilite
+CREATE TRIGGER Before_Reservation_Insert    -- Nom du déclencheur
+BEFORE INSERT ON Reservation              -- Déclencheur avant l'insertion dans la table Reservation
+FOR EACH ROW                       -- Pour chaque ligne insérée
+BEGIN                        -- Début du bloc
+    DECLARE AvailCount INT;           -- Variable pour stocker le nombre de disponibilités
+    SELECT COUNT(*)  
+    INTO AvailCount  -- Stocker le nombre de disponibilités
+    FROM Disponibilite  -- 
     WHERE ID_Materiel = NEW.Materiel_ID
-    AND NEW.DateDebutReservation BETWEEN DateDebut AND DateFin
-    AND NEW.DateFinReservation BETWEEN DateDebut AND DateFin;
+    AND NEW.DateDebutReservation BETWEEN DateDebut AND DateFin  -- Vérifier si la date de début est entre la date de début et de fin
+    AND NEW.DateFinReservation BETWEEN DateDebut AND DateFin;  -- Vérifier si la date de fin est entre la date de début et de fin
     
     IF AvailCount = 0 THEN
         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Le matériel n''est pas dispo pour cette période.';
@@ -38,15 +38,15 @@ DELIMITER ;
 
 DELIMITER $$
 
-CREATE TRIGGER Before_Disponibilite_Update 
-BEFORE UPDATE ON Disponibilite
-FOR EACH ROW
+CREATE TRIGGER Before_Disponibilite_Update  -- Nom du déclencheur
+BEFORE UPDATE ON Disponibilite          -- Déclencheur avant la mise à jour dans la table Disponibilite
+FOR EACH ROW                     -- Pour chaque ligne mise à jour
 BEGIN
-    DECLARE ResCount INT;
-    SELECT COUNT(*) INTO ResCount FROM Reservation
-    WHERE Materiel_ID = NEW.ID_Materiel
-    AND (NEW.DateDebut BETWEEN DateDebutReservation AND DateFinReservation
-        OR NEW.DateFin BETWEEN DateDebutReservation AND DateFinReservation
+    DECLARE ResCount INT;         -- Variable pour stocker le nombre de réservations
+    SELECT COUNT(*) INTO ResCount FROM Reservation     -- Compter le nombre de réservations
+    WHERE Materiel_ID = NEW.ID_Materiel    -- Vérifier les réservations pour le matériel mis à jour
+    AND (NEW.DateDebut BETWEEN DateDebutReservation AND DateFinReservation    -- Vérifier si la nouvelle période de disponibilité se chevauche avec des réservations existantes
+        OR NEW.DateFin BETWEEN DateDebutReservation AND DateFinReservation    
         OR (NEW.DateDebut <= DateDebutReservation AND NEW.DateFin >= DateFinReservation));
     
     IF ResCount > 0 THEN
@@ -63,13 +63,13 @@ DELIMITER ;
 
 DELIMITER $$
 
-CREATE TRIGGER Before_Disponibilite_Delete 
-BEFORE DELETE ON Disponibilite
+CREATE TRIGGER Before_Disponibilite_Delete   -- Nom du déclencheur
+BEFORE DELETE ON Disponibilite        -- Déclencheur avant la suppression dans la table Disponibilite
 FOR EACH ROW
-BEGIN
-    DECLARE ResCount INT;
-    SELECT COUNT(*) INTO ResCount FROM Reservation
-    WHERE ID_Disponibilite = OLD.ID_Disponibilite;
+BEGIN                    -- Pour chaque ligne supprimée
+    DECLARE ResCount INT;     -- Variable pour stocker le nombre de réservations
+    SELECT COUNT(*) INTO ResCount FROM Reservation   -- Compter le nombre de réservations
+    WHERE ID_Disponibilite = OLD.ID_Disponibilite;   -- Vérifier les réservations associées à la disponibilité supprimée
     
     IF ResCount > 0 THEN
         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Impossible de supprimer cette disponibilité car elle est associée à des réservations existantes.';
